510cba375e99a11363cba317bc83aedbac7eed59
Hammertime6901 <72567264+Hammertime6901@users.noreply.github.com>
2021-06-14 19:09:59 +0200

    Update language EN and NL
    registreer pagina vertaald en aangepast.
    en.php en nl.php registreer data toegevoegd.
    usercontroller.php aangepast ivm error messages bij foute invoer.

--- src/Views/languages/nl.php
+++ src/Views/languages/nl.php
@@ -10,45 +10,89 @@
 	
 	// eerst lang + controller + variable 
 	// Home pagina
-	$langTitle                   = "Home";
-	$langContent                 = "welkom bij de site van VitaeBikes";
+	$langTitle                         = "Home";
+	$langContent                       = "welkom bij de site van VitaeBikes";
 
 	// Navbar
 
 	// Contact pagina
-	$langContactTitle			 = 'Contact';
-	$langContactContent			 = 'Heb je een vraag of opmerking? Neem contact met ons op!';
+	$langContactTitle			       = 'Contact';
+	$langContactContent			       = 'Heb je een vraag of opmerking? Neem contact met ons op!';
 
-	$langContactFirstName 		 = 'Voornaam';
-	$langContactLastName 		 = 'Achternaam';
-	$langContactEmail 			 = 'Email';
-	$langContactComment 		 = 'Bericht';
-	$langContactButton 			 = 'Verzend';
+	$langContactFirstName 		       = 'Voornaam';
+	$langContactLastName 		       = 'Achternaam';
+	$langContactEmail 			       = 'Email';
+	$langContactComment 		       = 'Bericht';
+	$langContactButton 			       = 'Verzend';
 
 	// User Login pagina
-	$langUserLoginTitle          = 'Inloggen';
-	$langUserLoginContent        =  'Vul uw login gegevens in om aan te melden';
+	$langUserLoginTitle                = 'Inloggen';
+	$langUserLoginContent              =  'Vul uw login gegevens in om aan te melden';
 	
-	$langUserLoginEmail          = 'Gebruikersnaam';
-	$langUserLoginEmailPH        = 'E-mail adres';
-	$langUserLoginEmailErr       = [1 => 'Vul aub email adres in',
-							        2 => 'Onvolledig email adres, gebruik @ en .',
-							        3 => 'Geen gebruiker gevonden'];
+	$langUserLoginEmail                = 'Gebruikersnaam';
+	$langUserLoginEmailPH              = 'E-mail adres';
+	$langUserLoginEmailErr             = [1 => 'Vul aub email adres in',
+										  2 => 'Onvolledig email adres, gebruik @ en .',
+							              3 => 'Geen gebruiker gevonden'];
 	
-	$langUserLoginPassword       = 'Wachtwoord';
-	$langUserLoginPasswordPH     = 'Wachtwoord';
-	$langUserLoginPasswordErr    = [1 => 'Vul aub wachtwoord in',
-								    2 => 'Wachtwoord niet correct'];
+	$langUserLoginPassword             = 'Wachtwoord';
+	$langUserLoginPasswordPH           = 'Wachtwoord';
+	$langUserLoginPasswordErr          = [1 => 'Vul aub wachtwoord in',
+								          2 => 'Wachtwoord niet correct'];
 	
-	$langUserLoginButton         = 'Inloggen';
-	$langUserLoginRegister       = 'Nog geen account? Registreer';
+	$langUserLoginButton               = 'Inloggen';
+	$langUserLoginRegister             = 'Nog geen account? Registreer';
 	
 
-	
-	
-	
 	// User Registreer pagina
+	$langUserRegisterTitle             = 'Registreer';
+	$langUserRegisterContent           = 'Registreer als nieuwe klant';
 
+	$langUserRegisterEmail			   = 'E-mail adres';
+	$langUserRegisterEmailErr          = [ 1 => 'Vul aub email adres in',
+										   2 => 'Onvolledig email adres, gebruik @ en .',
+										   3 => 'Email al in gebruik'];
+
+	$langUserRegisterFirstname         = 'Voornaam';
+	$langUserRegisterFirstnameErr      = 'Vul aub voornaam in';
+
+	$langUserRegisterLastname	       = 'Achternaam';
+	$langUserRegisterLastnameErr       = 'Vul aub achternaam in';
+
+	$langUserRegisterStreet			   = 'Straat';
+	$langUserRegisterStreetErr		   = 'Vul aub woonplaats in';
+
+	$langUserRegisterHouseNumber       = 'Huisnr';
+	$langUserRegisterHouseNumberErr    = 'Vul aub huisnummer in';
+
+	$langUserRegisterPostalcode	 	   = 'Postcode';
+	$langUserRegisterPostalcodeErr 	   = [1 => 'Vul aub postcode in',
+										  2 => 'Geen spaties in de postcode gebruiken',
+										  3 => 'Postcode maximaal 6 karakters'];
+
+	$langUserRegisterCity			   = 'Woonplaats';
+	$langUserRegisterCityErr		   = 'Vul aub woonplaats in';
+
+	$langUserRegisterTelephone		   = 'Telefoon';
+	$langUserRegisterTelephoneErr	   = 'Vul aub telefoonnummer in';
+	
+	$langUserRegisterPassword		   = 'Wachtwoord';
+	$langUserRegisterPasswordErr	   = [1 => 'Vul aub wachtwoord in',
+									      2 => 'Wachtwoord moet minimaal 6 karakters zijn',
+										  3 => 'Wachtwoord moet een cijfer bevatten',
+										  4 => 'Wachtwoord moet een letter bevatten',
+										  5 => 'Wachtwoord moet een hoofdletter bevatten'];
+	
+	$langUserRegisterPasswordCheck	   = 'Wachtwoord controle';
+	$langUserRegisterPasswordCheckErr  = [1 => 'Vul aub wachtwoord ter controle in',
+										  2 => 'Wachtwoorden zijn niet gelijk'];
+
+	$langUserRegisterButton            = 'Registreer';
+	$langUserRegisterLogin             = 'Al een account? Login'; 
+	
+
+
+	
 
 
 ?>
