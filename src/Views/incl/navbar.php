86b42cbf8a301fb2ddd22328e4a6eb617545a864
Outis073 <72567157+Outis073@users.noreply.github.com>
2021-06-15 22:10:51 +0200

    a

--- src/Views/incl/navbar.php
+++ src/Views/incl/navbar.php
@@ -7,15 +7,15 @@
 
         <div class="collapse navbar-collapse" id="navbarsExampleDefault">
             <ul class="navbar-nav mr-auto">
-                <?php if(!isset($_SESSION['user_type']) || $_SESSION['user_type'] == "customer") : ?>
+                <?php if (!isset($_SESSION['user_type']) || $_SESSION['user_type'] == "customer") : ?>
                     <li class="nav-item">
                         <a class="nav-link" href="<?= 'index.php' ?>"><?= $langNavbarHome ?></a>
                     </li>
-                        <?php if(isset($_SESSION['user_type']) && $_SESSION['user_type'] == "customer") : ?>
-                            <li class="nav-item">
-                                <a class="nav-link" href="<?= 'index.php?controller=Order&action=index' ?>"><?= $langNavbarOrders ?></a>
-                            </li>
-                        <?php endif; ?>
+                    <?php if (isset($_SESSION['user_type']) && $_SESSION['user_type'] == "customer") : ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?= 'index.php?controller=Order&action=index' ?>"><?= $langNavbarOrders ?></a>
+                        </li>
+                    <?php endif; ?>
                     <li class="nav-item">
                         <a class="nav-link" href="<?= 'index.php?controller=Contact&action=index' ?>"><?= $langNavbarContact ?></a>
                     </li>
@@ -28,14 +28,14 @@
                     </li>
                 <?php endif; ?>
             </ul>
-            <?php if(!isset($_SESSION['user_type']) || $_SESSION['user_type'] == "customer") : ?>
+            <?php if (!isset($_SESSION['user_type']) || $_SESSION['user_type'] == "customer") : ?>
                 <div class="search-container">
                     <form class="search" action="./?controller=search" method="POST">
-                        <input class="" type="text" placeholder="<?= $langNavbarSearch ?>" name="search" />
+                        <input type="text" placeholder="<?= $langNavbarSearch ?>" name="search" />
                         <button type="submit"><i class="fa fa-search"></i></button>
                     </form>
                 </div>
-            <?php endif; ?> 
+            <?php endif; ?>
 
             <ul class="navbar-nav ml-auto">
                 <?php if (isset($_SESSION['user_id'])) : ?>
@@ -53,17 +53,18 @@
                         <a class="nav-link" href="./?controller=user&action=login"><?= $langNavbarLogin ?></a>
                     </li>
                 <?php endif; ?>
-                <?php if(!isset($_SESSION['user_type']) || $_SESSION['user_type'] == "customer") : ?>
-                   <form method="post" action="index.php?controller=Home&action=changeLanguage">
-                       <input type="hidden" name="language" value="en">
-                       <button type="submit" name="submit_button" class="btn btn-primary">English</button>   
-                    </form>
-                    
-                    <form method="post" action="index.php?controller=Home&action=changeLanguage">
-                       <input type="hidden" name="language" value="nl">
-                       <button type="submit" name="submit_button" class="btn btn-primary">Nederlands</button>
-                    </form>
-                <?php endif; ?> 
+                <?php if (!isset($_SESSION['user_type']) || $_SESSION['user_type'] == "customer") : ?>
+                    <div class="navbar-nav ml-auto">
+                        <form class="nav-item" method="post" action="index.php?controller=Home&action=changeLanguage">
+                            <input type="hidden" name="language" value="en">
+                            <button type="submit" name="submit_button" class="btn btn-taal">English</button>
+                        </form>
+                        <form class="nav-item" method="post" action="index.php?controller=Home&action=changeLanguage">
+                            <input type="hidden" name="language" value="nl">
+                            <button type="submit" name="submit_button" class="btn btn-taal">Nederlands</button>
+                        </form>
+                    </div>
+                <?php endif; ?>
             </ul>
         </div>
     </div>
