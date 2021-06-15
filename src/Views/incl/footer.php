6058793b54198cda0c825dfb219cae97a5a7129e
Outis073 <72567157+Outis073@users.noreply.github.com>
2021-06-15 22:32:54 +0200

    a

--- src/Views/incl/footer.php
+++ src/Views/incl/footer.php
@@ -1,7 +1,45 @@
-      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+<footer class="container-fluid bg-dark text-light">
+       <div class="row">
+              <div class="col-md-4">
+                     <h3>Vita<span>E-Bikes</span></h3>
+                     <p class="footer-links">
+                            <a href="<?= 'index.php' ?>"><?= $langNavbarHome ?></a> |
+                            <a href="<?= 'index.php?controller=Order&action=index' ?>"><?= $langNavbarOrders ?></a> |
+                            <a href="<?= 'index.php?controller=Contact&action=index' ?>"><?= $langNavbarContact ?></a>
 
-      </body>
+                     </p>
+                     <p class="footer-bedrijfsnaam">Vita E-bikes © 2020</p>
+              </div>
+              <div class="col-md-4">
+                     <div>
+                            <p><i class="fa fa-map-marker"></i> <span>Hogeschoollaan 0A</span> Noort-brabant, Nederland</p>
+                     </div>
+                     <div>
+                            <p><i class="fa fa-phone"></i> +31 6-12345678</p>
+                     </div>
+                     <div>
+                            <p><i class="fa fa-envelope"></i> <a href="mailto:info@vitabikes.nl">info@vitabikes.nl</a></p>
+                     </div>
+              </div>
+              <div class="col-md-4">
+                     <p class="footer-over-ons">
+                            <span>Over Vita</span>
+                            Vita e-bikes streeft naar een toegankelijke manier van transport voor iedere laag uit de samenleving om zo bij te kunnen dragen aan een optimale gezondheid.
+                     </p>
+                     <div class="footer-icons">
+                            <a class="footer-icon" href="#"><i class="fa fa-facebook"></i></a>
+                            <a class="footer-icon" href="#"><i class="fa fa-twitter"></i></a>
+                            <a class="footer-icon" href="#"><i class="fa fa-linkedin"></i></a>
+                            <a class="footer-icon" href="#"><i class="fa fa-github"></i></a>
+                     </div>
+              </div>
+       </div>
+</footer>
 
-      </html>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
