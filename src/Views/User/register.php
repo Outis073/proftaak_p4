b267d901843a994f836b67f3d38a5532439a5029
Outis073 <72567157+Outis073@users.noreply.github.com>
2021-06-16 17:07:55 +0200

    qa

--- src/Views/User/register.php
+++ src/Views/User/register.php
@@ -18,44 +18,44 @@
                     <form action="index.php?controller=User&action=register" method="POST" class="row">
 
                         <div class="col-md-12">
-                            <input type="text" name="email" class="form-control px-0 mb-4" <?php echo (!empty($email_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $email; ?>" placeholder="<?= $langUserRegisterEmail ?>" autofocus>
+                            <input type="text" name="email" class="form-control px-0 mb-4 <?php echo (!empty($email_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $email; ?>" placeholder="<?= $langUserRegisterEmail ?>" autofocus>
                             <span class="invalid-feedback"><?= $langUserRegisterEmailErr[$email_err] ?></span>
                         </div>
                         <div class="col-md-6">
-                            <input type="text" name="firstName" class="form-control px-0 mb-4" <?php echo (!empty($firstName_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $firstName; ?>" placeholder="<?= $langUserRegisterFirstname ?>">
+                            <input type="text" name="firstName" class="form-control px-0 mb-4 <?php echo (!empty($firstName_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $firstName; ?>" placeholder="<?= $langUserRegisterFirstname ?>">
                             <span class="invalid-feedback"><?= $langUserRegisterFirstnameErr ?></span>
                         </div>
                         <div class="col-md-6">
-                            <input type="text" name="lastName" class="form-control px-0 mb-4" <?php echo (!empty($lastName_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $lastName; ?>" placeholder="<?= $langUserRegisterLastname ?>">
+                            <input type="text" name="lastName" class="form-control px-0 mb-4 <?php echo (!empty($lastName_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $lastName; ?>" placeholder="<?= $langUserRegisterLastname ?>">
                             <span class="invalid-feedback"><?= $langUserRegisterLastnameErr ?></span>
                         </div>
                         <div class="col-md-8">
-                            <input type="text" name="street" id="street" class="form-control px-0 mb-4" <?php echo (!empty($street_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $street; ?>" placeholder="<?= $langUserRegisterStreet ?>">
+                            <input type="text" name="street" id="street" class="form-control px-0 mb-4 <?php echo (!empty($street_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $street; ?>" placeholder="<?= $langUserRegisterStreet ?>">
                             <span class="invalid-feedback"><?= $langUserRegisterStreetErr ?></span>
                         </div>
 
                         <div class="col-md-4">
-                            <input type="text" name="houseNumber" id="houseNumber" class="form-control px-0 mb-4" <?php echo (!empty($houseNumber_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $houseNumber; ?>" placeholder="<?= $langUserRegisterHouseNumber ?>">
+                            <input type="text" name="houseNumber" id="houseNumber" class="form-control px-0 mb-4 <?php echo (!empty($houseNumber_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $houseNumber; ?>" placeholder="<?= $langUserRegisterHouseNumber ?>">
                             <span class="invalid-feedback"><?= $langUserRegisterHouseNumberErr ?></span>
                         </div>
                         <div class="col-md-4">
-                            <input type="text" name="postalCode" id="postalCode" class="form-control px-0 mb-4" <?php echo (!empty($postalCode_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $postalCode; ?>" placeholder="<?= $langUserRegisterPostalcode ?>">
+                            <input type="text" name="postalCode" id="postalCode" class="form-control px-0 mb-4 <?php echo (!empty($postalCode_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $postalCode; ?>" placeholder="<?= $langUserRegisterPostalcode ?>">
                             <span class="invalid-feedback"><?= $langUserRegisterPostalcodeErr[$postalCode_err] ?></span>
                         </div>
                         <div class="col-md-8">
-                            <input type="text" name="city" id="city" class="form-control px-0 mb-4" <?php echo (!empty($city_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $city; ?>" placeholder="<?= $langUserRegisterCity ?>">
+                            <input type="text" name="city" id="city" class="form-control px-0 mb-4 <?php echo (!empty($city_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $city; ?>" placeholder="<?= $langUserRegisterCity ?>">
                             <span class="invalid-feedback"><?= $langUserRegisterCityErr ?></span>
                         </div>
                         <div class="col-md-12">
-                            <input type="text" name="telephone" id="telephone" class="form-control px-0 mb-4" <?php echo (!empty($telephone_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $telephone; ?>" placeholder="<?= $langUserRegisterTelephone ?>">
+                            <input type="text" name="telephone" id="telephone" class="form-control px-0 mb-4 <?php echo (!empty($telephone_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $telephone; ?>" placeholder="<?= $langUserRegisterTelephone ?>">
                             <span class="invalid-feedback"><?= $langUserRegisterTelephoneErr ?></span>
                         </div>
                         <div class="col-md-12">
-                            <input type="password" name="password" id="password" class="form-control px-0 mb-4" <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>" placeholder="<?= $langUserRegisterPassword ?>">
+                            <input type="password" name="password" id="password" class="form-control px-0 mb-4 <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>" placeholder="<?= $langUserRegisterPassword ?>">
                             <span class="invalid-feedback"><?= $langUserRegisterPasswordErr[$password_err] ?></span>
                         </div>
                         <div class="col-md-12 mb-md-4">
-                            <input type="password" name="passwordCheck" id="passwordCheck" class="form-control px-0 mb-4" <?php echo (!empty($passwordCheck_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $passwordCheck; ?>" placeholder="<?= $langUserRegisterPasswordCheck ?>">
+                            <input type="password" name="passwordCheck" id="passwordCheck" class="form-control px-0 mb-4 <?php echo (!empty($passwordCheck_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $passwordCheck; ?>" placeholder="<?= $langUserRegisterPasswordCheck ?>">
                             <span class="invalid-feedback"><?= $langUserRegisterPasswordCheckErr[$passwordCheck_err] ?></span>
                         </div>
                         <div class="row mt-md-4">
