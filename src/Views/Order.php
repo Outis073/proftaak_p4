4dd8740ebbf4920b2012de5d0eb6937e423bb56b
Outis073 <72567157+Outis073@users.noreply.github.com>
2021-06-16 19:02:56 +0200

    Update Order.php

--- src/Views/Order.php
+++ src/Views/Order.php
@@ -1,36 +1,40 @@
-<?php require_once (__DIR__ . '/incl/header.php'); ?>
+<?php require_once(__DIR__ . '/incl/header.php'); ?>
 
-<h1><?php echo $langOrderGreeting .' '. $_SESSION['user_name']; ?></h1>
-
-<br>
-<br>
-<p><?php echo $langOrderInfo?></p>
-<br>
-<br>
+<div class="container text-center">
+  <div class="row justify-content-center dots-row">
+    <h1 class="col-md-12 pb-2"><?php echo $langOrderGreeting . ' ' . $_SESSION['user_name']; ?></h1>
+    <p class="col-md-12 lead"><?php echo $langOrderInfo ?></p>
+  </div>
 
 
-<table>
-  <thead>
-    <tr>
-      <td>id</td>
-      <td>date</td>
-      <td>delivery_date</td>
-      <td>payment_option</td>
-      <td>status</td>
-    </tr>
-  </thead>
-  <tbody>
-    <?php foreach ($orders as $order) : ?>
-      <tr>
-        <td><?php echo $order->get('id'); ?></td>
-        <td><?php echo $order->get('date'); ?></td>
-        <td><?php echo $order->get('delivery_date'); ?></td>
-        <td><?php echo $order->get('payment_option'); ?></td>
-        <td><?php echo $order->get('status'); ?></td>
-      </tr>
-    <?php endforeach; ?>
-  </tbody>
-</table>
+  <div class="container">
+    <table class="container table table-hover">
+      <thead>
+        <tr class="row pb-2">
+          <th class="col-md-2"><strong>ID</strong></th>
+          <th class="col-md-3"><strong>Date</strong></th>
+          <th class="col-md-3"><strong>Delivery date</strong></th>
+          <th class="col-md-2"><strong>Payment option</strong></th>
+          <th class="col-md-2"><strong>Status</strong></th>
 
 
-<?php require_once (__DIR__ . '/incl/footer.php'); ?>
+        </tr>
+      </thead>
+      <tbody>
+        <?php foreach ($orders as $order) : ?>
+          <tr class="row">
+            <td class="col-md-2"><?php echo $order->get('id'); ?><< /td>
+            <td class="col-md-2"><?php echo $order->get('date'); ?></td>
+            <td class="col-md-2"><?php echo $order->get('delivery_date'); ?></td>
+            <td class="col-md-2"><?php echo $order->get('payment_option'); ?></td>
+            <td class="col-md-2"><?php echo $order->get('status'); ?></td>
+          </tr>
+        <?php endforeach; ?>
+      </tbody>
+    </table>
+  </div>
+
+
+</div>
+
+<?php require_once(__DIR__ . '/incl/footer.php'); ?>
