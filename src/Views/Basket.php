8238f501eac8318b183bd45688f7e4b1e86aec6c
Outis073 <72567157+Outis073@users.noreply.github.com>
2021-06-16 17:29:55 +0200

    Update Basket.php

--- src/Views/Basket.php
+++ src/Views/Basket.php
@@ -1,51 +1,55 @@
-<?php require_once ('incl/header.php'); ?>
+<?php require_once('incl/header.php'); ?>
 
-<div class="jumbotron jumbotron-flud text-center">
-	<div class="container">
-		<h1 class="display-3"><?= $langHomeTitle; ?></h1>
-		<p class="lead"><?= $langHomeContent; ?></p>
-		<table>
-			<thead>
-				<tr>
-					<th>Model</th>
-					<th>Category</th>
-					<th>Image</th>
-					<th>Price</th>
-					<th></th>
+<div class="container text-center">
+	<div class="row justify-content-center dots-row">
+		<h1 class="col-md-12 p-2"><?= $title; ?></h1>
+		<p class="lead">De mooiste fietsen</p>
 
-				</tr>
-			</thead>
-
-			<tbody>
-				<?php foreach($bikes as $bike) : ?>
-					<tr>
-						<?php $price = $bike->get('model')->get('price'); ?>
-						<td><?php echo $bike->get('model')->get('name'); ?></td>
-						<td><img src="src/images/<?php echo $bike->get('model')->get('image'); ?>" alt="No image available"></td>
-						<td><?php echo $bike->get('model')->get('price'); ?></td>
-						<td>
-							<?php foreach($bike->get('options') as $option) : ?>
-								<?php echo $option->get('name') . "(" . $option->get('price') . ")";
-								$price += $option->get('price'); ?>
-							<?php endforeach; ?>
-						</td>
-						<td>Totaal prijs: <?php echo $price; ?></td>
-						<td>
-							<form method="post" action="index.php?controller=Basket&action=removeBike" enctype='multipart/form-data'>
-								<input type="hidden" name="id" value="<?= $bike->get('id') ?>">
-	                  			<input type='submit' value='Verwijder fiets' name='button'>
-                			</form>
-                		</td>
-					</tr>
-				<?php endforeach; ?>
-			</tbody>
-		</table>
-		<form method="post" action="index.php?controller=Basket&action=order" enctype='multipart/form-data'>
-  			<input type='submit' value='Bestel' name='button'>
-		</form>
 	</div>
+	<section class="section">
+		<div class="container">
+
+			<div class="row d-flex align-items-center">
+				<?php foreach ($bikes as $bike) : ?>
+
+					<?php $price = $bike->get('model')->get('price'); ?>
+
+					<div class="col-6 mb-4">
+						<h2><?php echo $bike->get('model')->get('name'); ?></h2>
+					</div>
+					<div class="col-6 mb-4"><img src="src/images/<?php echo $bike->get('model')->get('image'); ?>" alt="No image available"></div>
+
+					<div class="col-2"><strong>
+							<p>Prijs fiets:</p>€ <?php echo $bike->get('model')->get('price'); ?>
+						</strong></div>
+					<div class="col-5">
+						<p class="text-center"><strong>Opties:</strong></p>
+						<?php foreach ($bike->get('options') as $option) : ?>
+							<?php echo $option->get('name') . "(" . $option->get('price') . ")<br>";
+							$price += $option->get('price'); ?>
+						<?php endforeach; ?>
+					</div>
+					<div class="col-2"><strong>
+							<p>Totaal prijs:</p> € <?php echo $price; ?>
+						</strong></div>
+					<div class="col-3">
+						<form method="post" action="index.php?controller=Basket&action=removeBike" enctype='multipart/form-data'>
+							<input type="hidden" name="id" value="<?= $bike->get('id') ?>">
+							<input type='submit' value='Verwijder fiets' name='button' class="btn btn-main-admin-edit">
+						</form>
+					</div>
+
+
+				<?php endforeach; ?>
+			</div>
+			<div class="col-12 mt-4">
+				<form method="post" action="index.php?controller=Basket&action=order" enctype='multipart/form-data'>
+					<input type='submit' value='Bestel' name='button' class="btn btn-main">
+				</form>
+			</div>
+		</div>
+	</section>
 </div>
 
-<?php require_once ('incl/footer.php'); ?>
 
-
+<?php require_once('incl/footer.php'); ?>
